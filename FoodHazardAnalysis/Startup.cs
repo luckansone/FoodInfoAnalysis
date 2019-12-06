297b5bd1e823ed7f30c3ca075e49fbe35826cd6c
Helen Kravchuk <kravchukl2000@gmail.com>
2019-12-06 12:57:58 +0200

    Add swagger

--- FoodHazardAnalysis/Startup.cs
+++ FoodHazardAnalysis/Startup.cs
@@ -15,6 +15,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace FoodHazardAnalysis
 {
@@ -43,6 +44,15 @@ namespace FoodHazardAnalysis
             services.AddTransient<IService<Products>, ProductService>();
             services.AddSingleton<IService<Eadditives>, EAdditiveService>();
             services.AddTransient<IService<Eadditives>, EAdditiveService>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Test API",
+                    Description = "ASP.NET Core Web API"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +65,12 @@ namespace FoodHazardAnalysis
             }
             
             app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Test API V1");
+            });
         }
     }
 }
